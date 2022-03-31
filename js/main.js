183a33f6d462460249670a6a4ff62c3fef767822
chemacenturion <centurion.philadelphia@gmail.com>
2022-03-30 20:58:19 -0400

    finished button functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 // Start writing JavaScript here!
+const button = document.querySelector("button");
+const body = document.body;
+
+// button.addEventListener("click", (event) => {
+//   if (body.classList.contains("offsite-is-open")) {
+//     body.classList.remove("offsite-is-open");
+//   } else {
+//     body.classList.add("offsite-is-open");
+//   }
+// });
+
+button.addEventListener('click', event => {
+    body.classList.toggle('offsite-is-open')
+})
