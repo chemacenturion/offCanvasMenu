25be3122dbbe74ef835eb7bfe1b9c8cf6db797bd
chemacenturion <centurion.philadelphia@gmail.com>
2022-03-30 21:14:54 -0400

    rebuilt with minimal code referencing

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
-// Start writing JavaScript here!
-const button = document.querySelector("button");
-const body = document.body;
-
-// button.addEventListener("click", (event) => {
-//   if (body.classList.contains("offsite-is-open")) {
-//     body.classList.remove("offsite-is-open");
-//   } else {
-//     body.classList.add("offsite-is-open");
-//   }
-// });
+const button = document.querySelector('button')
+const body = document.body
 
 button.addEventListener('click', event => {
     body.classList.toggle('offsite-is-open')
